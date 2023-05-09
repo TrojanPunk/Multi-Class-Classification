794df2522e02f649fe21cab1b8eed0c77d384a08
CR1502 <craig.lionel2020@vitbhopal.ac.in>
2023-05-09 15:16:59 +0530

    updates

--- multilabel_classification.py
+++ multilabel_classification.py
@@ -1,45 +1,39 @@
+# Import required libraries
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from skmultilearn.problem_transform import ClassifierChain
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from sklearn.metrics import accuracy_score, hamming_loss
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import MultiLabelBinarizer
 
-# Load the emotions dataset
+# Load the data
 data = pd.read_csv('emotions.csv')
 
-# Split the data into features and labels
-X = data.iloc[:, :-6].values  # features
-y = data.iloc[:, -6:].values  # labels
+# Split data into features and labels
+X = data.iloc[:,:-6]
+y = data.iloc[:,-6:]
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Convert labels to binary format
+mlb = MultiLabelBinarizer()
+y = mlb.fit_transform(y.values)
 
-# Define the neural network model
-model = Sequential()
-model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(y_train.shape[1], activation='softmax'))
+# Define the model
+def create_model():
+    model = Sequential()
+    model.add(Dense(6, input_dim=X.shape[1], activation='relu'))
+    model.add(Dense(4, activation='relu'))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-# Train the classifier chain model using the neural network as the base classifier
-classifier = ClassifierChain(model)
+# Create the classifier
+classifier = KerasClassifier(build_fn=create_model, epochs=10, batch_size=10)
 
-# Compile the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Evaluate the classifier using k-fold cross validation
+kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+results = cross_val_score(classifier, X, y, cv=kfold)
 
-# Fit the model to the training data
-classifier.fit(X_train, y_train)
-
-# Make predictions on the testing data
-y_pred = classifier.predict(X_test)
-
-# Evaluate the model using accuracy and hamming loss metrics
-accuracy = accuracy_score(y_test, y_pred)
-hamming_loss = hamming_loss(y_test, y_pred)
-
-# Print the evaluation metrics
-print(f"Accuracy: {accuracy}")
-print(f"Hamming Loss: {hamming_loss}")
+# Print the results
+print("Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
