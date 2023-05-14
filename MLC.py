c5c77820c7da20870e0612c3b5431cc2f607379f
CR1502 <craig.lionel2020@vitbhopal.ac.in>
2023-05-14 12:00:37 +0530

    changes

--- MLC.py
+++ MLC.py
@@ -1,46 +1,27 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
-from keras.models import Sequential
-from keras.layers import Dense
-import Classifier_chain
+from sklearn.ensemble import RandomForestClassifier
+from Classifier_chain import *
 
-# Load the emotions dataset
-emotions_df = pd.read_csv('emotions.csv')
+# Downloading and loading the Yeast dataset
+yeast_df = pd.read_csv('emotions.csv')
 
-# Extract the features and labels
-X = emotions_df.iloc[:, :-6].values
-y = emotions_df.iloc[:, -6:].values
+# Splitting into X and y
+X = yeast_df.iloc[:, :-6]
+y = yeast_df.iloc[:, -6:]
 
-# Binarize the labels
-mlb = MultiLabelBinarizer()
-y = mlb.fit_transform(y)
- 
-
-# Split the data into training and testing sets
+# Splitting into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_train = y_train.values.ravel()
 
-# Create the ClassifierChain model
-base_model = Sequential()
-base_model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
-base_model.add(Dense(32, activation='relu'))
-base_model.add(Dense(y_train.shape[1], activation='sigmoid'))
+# Creating the custom classifier chain with RandomForestClassifier as base estimator
+classifier = MyClassifierChain(RandomForestClassifier(random_state=42))
 
-classifier_chain = Classifier_chain.ClassifierChain(base_model, order=[0, 1, 2, 3, 4, 5], random_state=42)
+# Fitting the classifier to the training data
+classifier.fit(X_train, y_train)
 
-# Fit the model on the training data
-classifier_chain.fit(X_train, y_train, epochs=50, batch_size=64, verbose=1)
+# Predicting the test set
+y_pred = classifier.predict(X_test)
 
-# Make predictions on the testing data
-y_pred = classifier_chain.predict(X_test)
-
-# Convert the binary predictions to multi-label format
-y_pred = mlb.inverse_transform(y_pred)
-
-# Convert the true labels to multi-label format
-y_test = mlb.inverse_transform(y_test)
-
-# Evaluate the model
-from sklearn.metrics import classification_report
-print(classification_report(y_test, y_pred, target_names=mlb.classes_))
+# Printing the accuracy score
+print('Accuracy score:', classifier.score(X_test, y_test))
