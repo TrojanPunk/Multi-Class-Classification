86ffd28f14c4dda2438ad34dadb6645e98066f85
CR1502 <craig.lionel2020@vitbhopal.ac.in>
2023-06-05 19:25:22 +0530

    updates

--- MLC.py
+++ MLC.py
@@ -1,21 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from Classifier_chain import *
+from classifierchain import classifierchain
 
 # Downloading and loading the Yeast dataset
-yeast_df = pd.read_csv('emotions.csv')
+df = pd.read_csv('emotions.csv')
 
 # Splitting into X and y
-X = yeast_df.iloc[:, :-6]
-y = yeast_df.iloc[:, -6:]
+X = df.iloc[:, :-6]
+y = df.iloc[:, -6:]
 
 # Splitting into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 y_train = y_train.values.ravel()
 
 # Creating the custom classifier chain with RandomForestClassifier as base estimator
-classifier = MyClassifierChain(RandomForestClassifier(random_state=42))
+classifier = classifierchain(RandomForestClassifier(random_state=42))
 
 # Fitting the classifier to the training data
 classifier.fit(X_train, y_train)
